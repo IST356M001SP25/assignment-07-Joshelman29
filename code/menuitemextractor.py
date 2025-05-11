bc221bf65219dd58f408d712ff590e9aa5645b0a
josh elman <jdelman@syr.edu>
2025-05-11 14:49:15 +0500

    implemented clean_price, clean_scraped_text and extract_menu_item methods in menuitemextractory.py

--- code/menuitemextractor.py
+++ code/menuitemextractor.py
@@ -5,15 +5,34 @@ from menuitem import FoodItem
 
 
 def clean_price(price:str) -> float:
-    pass # TODO  replace with code
+   return float(price.replace("$", "").replace(",", ""))
+
 
 def clean_scraped_text(scraped_text: str) -> list[str]:
-    pass # TODO  replace with code
+    raw_items = scraped_text.split("\n")
+    data_list = []
+    for item in raw_items:
+        if item in ['GS',"V","S","P"]:
+            continue
+        if item.startswith("NEW"):
+            continue
+        if len(item.strip()) == 0:
+            continue
 
-def extract_menu_item(title:str, scraped_text: str) -> MenuItem:
-    pass # TODO  replace with code
+        data_list.append(item)
+
+    return data_list
+
+def extract_menu_item(title:str, scraped_text: str) -> FoodItem:
+    cleaned_items = clean_scraped_text(scraped_text)
+    item = FoodItem(category=title, name="", price=0.0, description="")
+    item.name = cleaned_items[0]
+    item.price = clean_price(cleaned_items[1])
+    if len(cleaned_items) > 2:
+        item.description = cleaned_items[2]
+    else:
+        item.description = "No description available."
+    return item
 
 
 
-if __name__=='__main__':
-    pass
